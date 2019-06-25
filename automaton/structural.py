657aea02225872d3a96c13f5026c86db7cfaf2d1
jmbhughes <hughes.jmb@gmail.com>
2019-06-25 06:13:23 -0600

    added counting, immuneregenerator, and graph drawing

--- automaton/structural.py
+++ automaton/structural.py
@@ -3,14 +3,16 @@ from typing import Callable, List, Optional, Tuple
 from skimage.util import view_as_windows
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
+import networkx as nx
+from networkx.drawing.nx_agraph import to_agraph
 
 
 class StructuralAutomaton:
     def __init__(self, generation0: np.ndarray, initializer: Callable, graph: dict, functions: dict,
                  window_size: int, save_history: bool = True) -> None:
         """
-        Initialize a structural automaton. It is defined by the transition graph.
-         A transition can be taken if the boolean function on its edge returns true.
+        Initialize a structural automaton. It is defined by the transition graph. This indicates which states can
+        go to which states. A transition in the graph can be taken if the boolean function on its edge returns true.
         :param generation0: the array of initial states for the automaton.
         :param initializer: when the current state has to grow use this function to initialize the new cells
         :param graph: the transition graph that describes which states can go to which states, e.g.
@@ -28,6 +30,7 @@ class StructuralAutomaton:
         self.__functions = functions
         self.current_iteration = generation0.copy().astype(int)
         self.__next_iteration = self.current_iteration.copy()
+        self.generation = 0
 
         # process the window size
         self.__window_size = window_size
@@ -116,14 +119,21 @@ class StructuralAutomaton:
             self.add_transition(edge, function)
 
     def __iterate(self) -> None:
+        """
+        A helper function to complete one iteration
+        """
+        self.generation += 1
         # create a set of patches to iterate over
         patches = view_as_windows(self.current_iteration, (self.__window_size, self.__window_size))
+
+        # for each patch
         for i in range(patches.shape[0]):
             for j in range(patches.shape[1]):
+                # look at that patch
                 patch = patches[i, j]
-                state = patch[self.__window_size//2, self.__window_size//2]
+                state = patch[self.__window_size//2, self.__window_size//2]  # the center of the patch
 
-                # determine the new state
+                # determine the new state using the transition functions
                 for becomes in self.__graph[state]:
                     if self.__functions[state][becomes](patch):
                         self.__next_iteration[i + self.__window_size//2, j + self.__window_size//2] = becomes
@@ -143,11 +153,25 @@ class StructuralAutomaton:
         pass
 
     def iterate(self, n: int = 1) -> None:
+        """
+        Iterate the automaton forward
+        :param n: how many steps forward to go
+        """
         for _ in range(n):
             self.__iterate()
 
     def show(self, iteration: Optional[int] = None, view_size: Optional[int] = None,
              save: Optional[str] = None, color_table: Optional[dict] = None) -> None:
+        """
+        Show the automaton using Matplotlib as an image
+        :param iteration: which iteration to show
+        :param view_size: If None, shows the full grid.
+            Otherwise shows the view_size pixel square centered on the middle..
+        :param save:  Where to save the image. If None, it does not save.
+        :param color_table: How to color the automaton. Give a dictionary with keys of states and values of colors.
+            If None, uses Matplotlib's default
+        """
+        # find the iteration
         if iteration is not None:
             if self.history is False:
                 raise RuntimeError("Cannot plot old iteration since history is false.")
@@ -182,12 +206,40 @@ class StructuralAutomaton:
         else:
             fig.show()
 
-    def count(self, label: int) -> int:
-        pass
+    def count(self, labels: List[int], iteration: Optional[int] = None) -> int:
+        """
+        Count the number of cells in an iteration that have the specified labels
+        :param labels: which labels to count
+        :param iteration: the iteration to look at. If None, uses the current iteration.
+        :return: Number of cells with that label.
+        """
+        grid = self.current_iteration if iteration is None else self.history[iteration]
+        return sum([np.sum(grid == label) for label in labels])
+
+    def draw_graph(self, path: str) -> None:
+        """
+        Draws the transition graph using networkx and graphviz
+        :param path: where to save the image
+        """
+        g = nx.MultiDiGraph()
+        g.add_nodes_from(self.states())
+        g.add_edges_from(self.transitions())
+        a = to_agraph(g)
+        a.layout("dot")
+        a.draw(path)
 
 
 class UlamWarburtonAutomaton(StructuralAutomaton):
+    """
+    The standard Ulam Warburton automaton. This automaton works by using a window of 3x3. If exactly one cell
+    to the north, south, west, or east is on, then the cell turns on. Once a cell is on, it never turns off.
+    """
     def __init__(self, initial_grid: np.ndarray, save_history: bool = True):
+        """
+        Create an Ulam Warburton Automaton
+        :param initial_grid: The grid to begin with
+        :param save_history: whether to save iterations
+        """
         super().__init__(initial_grid, lambda n: 0,
                          {0: [0, 1], 1: [1]},
                          {0: {0: lambda patch: patch[0, 1] + patch[1, 0] + patch[1, 2] + patch[2, 1] != 1,
@@ -196,7 +248,17 @@ class UlamWarburtonAutomaton(StructuralAutomaton):
 
 
 class RegeneratingUWAutomaton(StructuralAutomaton):
+    """
+    Similar to the Ulam Warburton automaton except now on cells turn back off after being on for a specified number
+    of iterations. We do this by introducing new states that transition from the on.
+    """
     def __init__(self, initial_grid: np.ndarray, delay: int, save_history: bool = True):
+        """
+        Create a Regenerating Automaton
+        :param initial_grid: the first generation state
+        :param delay: how many generations an on cell stays on
+        :param save_history: whether to save iterations
+        """
         # setup the graph
         graph = {0: [0, 1]}
         for i in range(1, delay):
@@ -213,16 +275,69 @@ class RegeneratingUWAutomaton(StructuralAutomaton):
 
 
 class ImmuneUWAutomaton(StructuralAutomaton):
-    def __init__(self, initial_grid: np.ndarray, immunity: int, save_history: bool = True):
+    """
+    A very silly version of the Ulam Warburton automaton where cells never wait a delay time before turning on.
+    We do the same thing as in RegeneratingAutomaton
+    """
+    def __init__(self, initial_grid: np.ndarray, delay: int, save_history: bool = True):
+        """
+        Create an Immune Ulam Warburton Automaton
+        :param initial_grid: the first generation state
+        :param delay: how many generations an off cell must be off before activating
+        :param save_history: whether to save iterations
+        """
         # setup the graph
         graph = {0: [0, 1]}
-        for i in range(-immunity, 0):
+        for i in range(-delay, 0):
             graph[i] = [i+1]
 
         # setup the functions
         functions = {0: {0: lambda patch: patch[0, 1] + patch[1, 0] + patch[1, 2] + patch[2, 1] != 1,
                          1: lambda patch: patch[0, 1] + patch[1, 0] + patch[1, 2] + patch[2, 1] == 1}}
-        for i in range(-immunity, 0):
+        for i in range(-delay, 0):
             functions[i] = {i + 1: lambda patch: True}
-        super().__init__(initial_grid, lambda n: n-immunity if  n < immunity else 0, graph, functions, 3,
+        super().__init__(initial_grid, lambda n: n-delay if n < delay else 0, graph, functions, 3,
+                         save_history=save_history)
+
+
+class ImmuneRegeneratingUWAutomaton(StructuralAutomaton):
+    """
+    A combination between the ImmuneUWAutomaton and RegeneratingUWAutomaton. Cells both have a waiting period to
+    turn on and to turn off. We do this by lots of extra nodes. :D
+    """
+    def __init__(self, initial_grid: np.ndarray, on_period: int, off_period: int, save_history: bool = True):
+        """
+        Create this kind of automaton
+        :param initial_grid: the initial grid
+        :param on_period: how long an on cell stays on before deactivating
+        :param off_period: how long an off cell stays off before deactivating
+        :param save_history: whether to save previous iterations
+        """
+        # setup the graph
+        # basic UW
+        graph = {0: [0, 1]}
+
+        # these are the additional states for on_period generations before shutting back off
+        for i in range(1, on_period):
+            graph[i] = [i+1]
+        graph[on_period] = -off_period
+
+        # these are the additional states for off_period generations where a cell is immune and won't turn on
+        for i in range(-off_period, 0):
+            graph[i] = [i+1]
+
+        # setup th functions
+        # the basic UW
+        functions = {0: {0: lambda patch: patch[0, 1] + patch[1, 0] + patch[1, 2] + patch[2, 1] != 1,
+                         1: lambda patch: patch[0, 1] + patch[1, 0] + patch[1, 2] + patch[2, 1] == 1}}
+
+        # functions for on transitions
+        for i in range(1, on_period):
+            functions[i] = {i+1: lambda patch: True}
+        functions[on_period] = {0: lambda patch: True}
+
+        # functions for off transitions
+        for i in range(-off_period, 0):
+            functions[i] = {i+1: lambda patch: True}
+        super().__init__(initial_grid, lambda n: n-off_period if n < off_period else 0, graph, functions, 3,
                          save_history=save_history)
